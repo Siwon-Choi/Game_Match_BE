7ef4a57a412c488344ea71011ee7c750bb1bce86
Siwon-Choi <c000317@naver.com>
2026-07-04 19:51:12 +0900

    #1 [Feat] : 프로젝트 시작

---
+++ src/main/java/com/hd/gamematch/GameMatchApplication.java
@@ -0,0 +1,13 @@
+package com.hd.gamematch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameMatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameMatchApplication.class, args);
+    }
+
+}
